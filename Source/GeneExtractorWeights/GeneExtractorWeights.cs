38c4e24ff1d5d9ed5a2ac305d24949626cc05885
Daniel Wedemeyer <daniel.wedemeyer@web.de>
2022-11-03 12:26:04 +0100

    Add files via upload

---
+++ Source/GeneExtractorWeights/GeneExtractorWeights.cs
@@ -0,0 +1,112 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using HarmonyLib;
+using RimWorld;
+using UnityEngine;
+using Verse;
+
+namespace GeneExtractorWeights;
+
+public class GeneExtractorWeights : Mod
+{
+    private static List<GeneDef> cachedGeneDefsInOrder;
+    private readonly GeneExtractorWeightsSettings _settings;
+    private List<GeneWeight> _genes;
+    private float _scrollHeight;
+
+    private Vector2 _scrollPosition = new(0, 0);
+
+    public GeneExtractorWeights(ModContentPack pack) : base(pack)
+    {
+        var harmony = new Harmony("ObiVayneKenobi.GeneExtractorWeights");
+        harmony.PatchAll();
+
+        _settings = GetSettings<GeneExtractorWeightsSettings>();
+
+
+        Log.Message("GeneExtractorWeights loaded");
+    }
+
+    internal static List<GeneDef> GenesInOrder
+    {
+        get
+        {
+            if (cachedGeneDefsInOrder == null)
+            {
+                cachedGeneDefsInOrder = new List<GeneDef>();
+                foreach (var allDef in DefDatabase<GeneDef>.AllDefs)
+                    //if (allDef.endogeneCategory != EndogeneCategory.Melanin)
+                    cachedGeneDefsInOrder.Add(allDef);
+                cachedGeneDefsInOrder.SortBy(x => -x.displayCategory.displayPriorityInXenotype,
+                    x => x.displayCategory.label, x => x.displayOrderInCategory);
+            }
+
+            return cachedGeneDefsInOrder;
+        }
+    }
+
+    public override void DoSettingsWindowContents(Rect inRect)
+    {
+        if (!_settings._isInitialized) _settings.InitializeGenes();
+
+        if (_genes is null)
+        {
+            _genes = _settings.Genes.Values.ToList();
+            _genes.Sort((a, b) => GenesInOrder.IndexOf(GenesInOrder.First(g =>
+                string.Equals(g.defName, a.GeneDefName, StringComparison.Ordinal))) - GenesInOrder.IndexOf(
+                GenesInOrder.First(g =>
+                    string.Equals(g.defName, b.GeneDefName, StringComparison.Ordinal))));
+        }
+
+        const float geneIconSize = 90;
+
+        var listingStandard = new Listing_Standard();
+        listingStandard.Begin(inRect);
+        listingStandard.CheckboxLabeled("Ignore metabolism limit", ref _settings._ignoreMetabolismLimit, height: 24,
+            tooltip:
+            "by default, the gene extractor will not add a gene to the extracted genepack if it would exceed the metabolism limit of the pawn. This option will ignore that limit.");
+        Widgets.BeginScrollView(inRect with { y = 40, height = inRect.height - 76 }, ref _scrollPosition,
+            new Rect(0.0f, 0.0f, inRect.width - 16f, _scrollHeight));
+
+        float y = 0;
+        var left = true;
+        foreach (var gene in _genes)
+        {
+            var geneDef = DefDatabase<GeneDef>.GetNamedSilentFail(gene.GeneDefName);
+            if (geneDef == null) continue;
+            GeneUIUtility.DrawGeneDef(geneDef,
+                new Rect(left ? 0 : inRect.width / 2, y - _scrollPosition.y, geneIconSize, geneIconSize),
+                GeneType.Endogene, null);
+            Widgets.HorizontalSlider(
+                new Rect(geneIconSize + 24 + (left ? 0 : inRect.width / 2),
+                    y + geneIconSize / 2 - 12 - _scrollPosition.y, inRect.width / 2 - geneIconSize - 48, 24),
+                ref gene.Weight, new FloatRange(0, 10), roundTo: .1f);
+            Widgets.Label(
+                new Rect(geneIconSize + 24 + (left ? 0 : inRect.width / 2),
+                    y + geneIconSize / 2 + 12 - _scrollPosition.y, inRect.width / 2 - geneIconSize - 48, 24),
+                gene.Weight.ToString("0"));
+            if (!left)
+                y += geneIconSize + 6;
+            left = !left;
+        }
+
+        if (Event.current.type == EventType.Layout)
+            _scrollHeight = (y + inRect.height) / 2;
+        Widgets.EndScrollView();
+
+        if (Widgets.ButtonText(new Rect(inRect.width - 100, inRect.height - 30, 100, 30), "Reset"))
+        {
+            Log.Message("Resetting gene weights");
+            _settings.ResetToDefault();
+        }
+
+        listingStandard.End();
+        base.DoSettingsWindowContents(inRect);
+    }
+
+    public override string SettingsCategory()
+    {
+        return "Gene Extractor Weights";
+    }
+}
---
+++ Source/GeneExtractorWeights/GeneExtractorWeightsSettings.cs
@@ -0,0 +1,103 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using Verse;
+
+namespace GeneExtractorWeights;
+
+public class GeneWeight : IExposable
+{
+    public float Weight;
+
+
+    public GeneWeight(string geneDefName, float weight)
+    {
+        GeneDefName = geneDefName;
+        Weight = weight;
+    }
+
+    public GeneWeight()
+    {
+    }
+
+    public string GeneDefName { get; set; }
+
+    public void ExposeData()
+    {
+        var geneDefName = GeneDefName;
+        Scribe_Values.Look(ref geneDefName, nameof(GeneDefName));
+        GeneDefName = geneDefName;
+        var weight = Weight;
+        Scribe_Values.Look(ref weight, nameof(Weight), forceSave: true);
+        Weight = weight;
+    }
+}
+
+public class GeneExtractorWeightsSettings : ModSettings
+{
+    private Dictionary<string, GeneWeight> _genes = new();
+
+    internal bool _ignoreMetabolismLimit;
+    internal bool _isInitialized;
+
+    public IReadOnlyDictionary<string, GeneWeight> Genes => _genes;
+
+    public bool IgnoreMetabolismLimit => _ignoreMetabolismLimit;
+
+    internal void AddGene(GeneWeight gene)
+    {
+        _genes[gene.GeneDefName] = gene;
+    }
+
+    public void InitializeGenes()
+    {
+        foreach (var gene in GeneExtractorWeights.GenesInOrder)
+        {
+            if (Genes.ContainsKey(gene.defName))
+                continue;
+
+            AddGene(new GeneWeight(gene.defName, GetDefaultWeight(gene)));
+        }
+
+        foreach (var gene in Genes.Values.ToList())
+            if (GeneExtractorWeights.GenesInOrder.All(g => g.defName != gene.GeneDefName))
+            {
+                Log.Warning(
+                    $"GeneExtractorWeights: Gene {gene.GeneDefName} is not in the list of genes. Removing it from the list.");
+                _genes.Remove(gene.GeneDefName);
+            }
+
+        _genes = _genes
+            .OrderBy(g => GeneExtractorWeights.GenesInOrder.IndexOf(GeneExtractorWeights.GenesInOrder.First(gg =>
+                string.Equals(gg.defName, g.Value.GeneDefName, StringComparison.Ordinal))))
+            .ToDictionary(g => g.Key, g => g.Value);
+
+        _isInitialized = true;
+    }
+
+    private static int GetDefaultWeight(GeneDef gene)
+    {
+        return gene.biostatArc > 0 ? 0 : gene.biostatCpx > 0 ? 3 : 1;
+    }
+
+    public override void ExposeData()
+    {
+        Scribe_Values.Look(ref _ignoreMetabolismLimit, nameof(IgnoreMetabolismLimit));
+
+        var genes = Genes.Values.Select(g => new GeneWeight(g.GeneDefName, g.Weight)).ToList();
+        Scribe_Collections.Look(ref genes, nameof(Genes), LookMode.Deep);
+
+        _genes = genes.ToDictionary(g => g.GeneDefName);
+
+        base.ExposeData();
+    }
+
+    public void ResetToDefault()
+    {
+        foreach (var gene in Genes.Values)
+        {
+            var geneDef = DefDatabase<GeneDef>.GetNamed(gene.GeneDefName);
+            gene.Weight = GetDefaultWeight(geneDef);
+        }
+    }
+}
